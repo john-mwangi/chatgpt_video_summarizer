7b92e9f60f095b071ee6d35fbb55a3bd761eff05
John Mwangi <mail.mwangi@gmail.com>
2024-01-02 21:59:10 +0300

    Commence video summary

---
+++ src/summarize_video.py
@@ -0,0 +1,4 @@
+from langchain.chains import LLMChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
