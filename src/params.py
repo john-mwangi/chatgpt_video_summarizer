4b8aa073f90b566fb3dd2ee9cc7d69afe1b60d0a
John Mwangi <mail.mwangi@gmail.com>
2024-01-03 18:53:58 +0300

    Add params

--- src/params.py
+++ src/params.py
@@ -4,4 +4,7 @@ ROOT_DIR = Path(__file__).parent.parent.resolve()
 transcript_dir = ROOT_DIR / "transcripts"
 
 CHUNK_SIZE = 10
-LIMIT_CHUNKS = 3
+LIMIT_CHUNKS = 4
+SUMMARY_LIMIT = 100
+BULLETS = 3
+BATCH_CHUNKS = 2
