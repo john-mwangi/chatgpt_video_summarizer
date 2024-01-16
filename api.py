a0b73d3307784ca184d7f893ae3097891988f4c5
John Mwangi <mail.mwangi@gmail.com>
2024-01-16 11:48:26 +0300

    Implement api
    Closes #22

---
+++ api.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from main import main
+
+
+class VideoUrls(BaseModel):
+    channels: list[str]
+    videos: list[str]
+    LIMIT_TRANSCRIPT: float | int | None = 0.25
+    top_n: int = 2
+    sort_by: str = "newest"
+
+
+app = FastAPI()
+
+
+@app.post(path="/summarize_video")
+def fetch_video_summary(video_urls: VideoUrls):
+    summaries = main(
+        channels=video_urls.channels,
+        videos=video_urls.videos,
+        LIMIT_TRANSCRIPT=video_urls.LIMIT_TRANSCRIPT,
+        sort_by=video_urls.sort_by,
+    )
+
+    data = {"summaries": summaries}
+
+    return {
+        "data": data,
+        "status": "SUCCESS",
+        "message": "Video was successfully summarised",
+    }
